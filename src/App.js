e942fc653425fd1252a01ee22afa181a29f0bef2
Trainy32 <cjstkdthsu98@naver.com>
2022-06-10 21:52:53 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { Route, Routes, useNavigate } from 'react-router-dom'
+
+// 리덕스 관련 Imports
+import {useDispatch, useSelector} from 'react-redux'
+
+// CSS 관련 Imports
+import styled from 'styled-components'
+
+// 컴포넌트 Imports
+import AnimeList from "./AnimeList";
+
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+      
+      </Routes>
     </div>
   );
 }
